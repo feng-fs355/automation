9d5c3dd434101adac3b8a070c5698f458164ee4f
feng-fs355 <feng038100@gmail.com>
2024-01-29 14:18:15 +0800

    Modify and correct OPENAPI
    Modify and correct OPENAPI

--- pytestlab/test_api.py
+++ pytestlab/test_api.py
@@ -13,45 +13,21 @@ import webbrowser
 
 root_path = os.path.dirname(os.path.realpath(__file__))
 
-@pytest.mark.parametrize('user, passwd',
+@pytest.mark.parametrize('user, password',
                          [('min', 'abcdefgh'),
-                          ('tom', 'a123456a')])
-def test_passwd_md5(user, passwd):
-
+                          ('tom', 'a123456a'),
+                          ('luke', "21880377"),
+                          ('zeno-number1', "a4fruch11")])
+def test_passwd_md5(user, password):
+    # command example : echo -n a4fruch11 | md5sum
     db = {
         'min': 'e8dc4081b13434b45189a720b77b6818',
         'tom': '1702a132e769a623c1adb78353fc9503',
-        'luke': '1234567898',
-        'asus': '655888'
+        'luke': 'f544e1cd917e141842393d9d6a2874c4',
+        'zeno-number1': 'c0ff63591a87ad0d45d8ab93fdaa67fa'
     }
     import hashlib
-    assert hashlib.md5(passwd.encode()).hexdigest() == db[user]
-
-#def test_clients():
-    #/api/gen/clients
-#    print("Step 1 :  /api/gen/clients")
-#    return GeneralGet('/api/gen/clients')
-
-#def test_clientlanguage():
-    # /api/gen/clients/{language}
-#    print("Step 2 : /api/gen/clients/{language}",None,None)
-#    language = ('ada', 'android', 'apex')
-#    for item in language:
-#        time.sleep(1)
-#        result = JSONGet(f'/api/gen/clients/{item}',None, None)
-#        Data = result['sortParamsByRequiredFlag']
-#        OPT = Data['opt']
-#        print(f"opt is : {OPT} ")
-#        DES = Data['description']
-#        print((f"descrption is : {DES} "))
-#        TYPE = Data['type']
-#        print((f"TYPE is : {TYPE} "))
-#        OPTVAL = Data['optValue']
-#        print((f"optValue is : {OPTVAL} "))
-#        DEFAULT = Data['default']
-#        print((f"default is : {DEFAULT} "))
-#        ENUM = Data['enum']
-#        print((f"ENUM is : {ENUM} "))
+    assert hashlib.md5(password.encode()).hexdigest() == db[user]
 
 def test_nasaapod():
     # API : /planetary/apod
@@ -71,9 +47,34 @@ def test_nasaMarsWeather():
     Data = result['validity_checks']
     print(Data)
 
-
 def test_showtime():
     current = time.localtime()
     print(current)
 
+def test_clients():
+    #/api/gen/clients
+    # https://api.openapi-generator.tech/api/gen/clients
+    print("Step 1 :  /api/gen/clients")
+    return GeneralGet('/api/gen/clients')
 
+def test_clientlanguage():
+    # /api/gen/clients/{language}
+    #https://api.openapi-generator.tech/api/gen/clients/apex
+    print("Step 2 : /api/gen/clients/{language}",None,None)
+    language = ('ada', 'android', 'apex')
+    for item in language:
+        time.sleep(1)
+        result = JSONGet(f'/api/gen/clients/{item}',None, None)
+        Data = result['sortParamsByRequiredFlag']
+        OPT = Data['opt']
+        print(f"opt is : {OPT} ")
+        DES = Data['description']
+        print((f"descrption is : {DES} "))
+        TYPE = Data['type']
+        print((f"TYPE is : {TYPE} "))
+        OPTVAL = Data['optValue']
+        print((f"optValue is : {OPTVAL} "))
+        DEFAULT = Data['default']
+        print((f"default is : {DEFAULT} "))
+        ENUM = Data['enum']
+        print((f"ENUM is : {ENUM} "))
