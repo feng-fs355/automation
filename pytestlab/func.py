6092c926cfafc9bb208ae206dc0252c94ecccfa4
mhsiehraspberry <111551841+mhsiehraspberry@users.noreply.github.com>
2024-02-05 10:23:45 +0800

    python material check-in
    python material check-in

--- pytestlab/func.py
+++ pytestlab/func.py
@@ -1,13 +1,16 @@
 
 
 
-def main():
-    x = int(input("what is x? "))
-    print("x square is", square(x))
-def square(n):
-    return n * n
-if __name__=="__main__":
-    main()
+
+
+
+#def main():
+#    x = int(input("what is x? "))
+#    print("x square is", square(x))
+#def square(n):
+#    return n * n
+#if __name__=="__main__":
+#    main()
 
 
 """
