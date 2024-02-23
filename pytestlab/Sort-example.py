84471e256e1a2e4de660dab48059682937979c27
feng-fs355 <feng038100@gmail.com>
2024-02-23 15:48:59 +0800

    Update material for python course
    Update material for python course

---
+++ pytestlab/Sort-example.py
@@ -0,0 +1,7 @@
+time_list = [12, 2, 32, 19, 57, 22, 14]
+print(sorted(time_list))
+
+original_list = [12, 2, 32, 19, 57, 22, 14]
+sorted_list_descending = sorted(original_list, reverse=True)
+
+print(sorted_list_descending)
---
+++ python-class/ascending.py
@@ -0,0 +1,7 @@
+#purpuse: remark "Sort ascending by duplicating, it sorts them in ascending order."
+original_list = [12, 2, 32, 19, 57, 22, 14]
+sorted_list = sorted(original_list)
+
+print("Original list:", original_list)
+print("Sorted list in ascending order:", sorted_list)
+
